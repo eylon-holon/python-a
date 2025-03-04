cf0ad3e7501bd7ce4fed5da54a80977f92d7d876
gosha-smeh <gosha.smeh@gmail.com>
2025-03-04 05:43:27 +0200

    test turtle

--- turtle/jturtle.py
+++ turtle/jturtle.py
@@ -6,3 +6,22 @@ def Turtle(width = 500, height = 500):
     myTS = t3.TurtleScreen(myCanvas)
     myTS.clear()
     return t3.Turtle(myTS)
+
+def testIt():
+    t = Turtle()
+
+    t.color("blue")
+    t.pensize(3)
+    t.penup()
+    t.goto(-200, -50)
+    t.pendown()
+    for i in range(4):
+        t.forward(200)
+        t.left(90)
+    t.color("green")
+    t.pensize(10)
+    t.penup()
+    t.goto(-150, 50)
+    t.pendown()
+    t.goto(-100, 0)
+    t.goto(-30, 120)
